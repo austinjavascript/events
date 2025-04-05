e14e6dfcd196c9329f469e96baf48a1b7332704f
Suzanne Rozier <suzubara@gmail.com>
2025-04-05 16:43:31 -0500

    Add reveal.js, slide template

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from 'react'
+import Reveal from 'reveal.js'
+import Highlight from 'reveal.js/plugin/highlight/highlight'
+import Notes from 'reveal.js/plugin/notes/notes'
+import 'reveal.js/dist/reveal.css'
+import './styles/index.scss'
+import Splash from './slides/splash'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const deckDivRef = useRef<HTMLDivElement>(null) // reference to deck container div
+  const deckRef = useRef<Reveal.Api | null>(null) // reference to deck reveal instance
+
+  useEffect(() => {
+    // Prevents double initialization in strict mode
+    if (deckRef.current) return
+
+    deckRef.current = new Reveal(deckDivRef.current!, {
+      transition: 'slide',
+      // other config options
+    })
+
+    deckRef.current
+      .initialize({
+        plugins: [Highlight, Notes],
+        disableLayout: true,
+      })
+      .then(() => {
+        // good place for event handlers and plugin setups
+      })
+
+    return () => {
+      try {
+        if (deckRef.current) {
+          deckRef.current.destroy()
+          deckRef.current = null
+        }
+      } catch (e) {
+        console.warn('Reveal.js destroy call failed.')
+      }
+    }
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="reveal" ref={deckDivRef}>
+      <div className="slides">
+        <Splash />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementsByTagName('body')[0]).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )

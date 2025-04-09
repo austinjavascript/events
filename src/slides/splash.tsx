be0ce7d6f5d9b80925553f07ee07a13a83b3f96c
Joah Gerstenberg <me@joahg.com>
2025-04-08 20:44:51 -0500

    feat: add assets, finish splash screen

--- src/slides/splash.tsx
+++ src/slides/splash.tsx
@@ -4,23 +4,27 @@ const Splash = () => {
       <div className="quadrants">
         <div className="quadrant">
           <div className="quadrant-content">
-            <h1>Welcome to Austin JavaScript</h1>
+            <img src="/logo.png" />
+            <h1>Welcome to<br/>Austin JavaScript</h1>
             <p>we’re glad you’re here :)</p>
           </div>
         </div>
         <div className="quadrant">
           <div className="quadrant-content">
-            <h2>Join us on Discord</h2>
+            <img src="/discord-qr.png" width="300" />
+            <h2>Join us on <img src="/discord-mark-black.png" style={ { margin: '-5px 0', height: 30 } }/> Discord</h2>
           </div>
         </div>
         <div className="quadrant">
           <div className="quadrant-content">
-            <h3>sponsored by</h3>
+            <img src="/shirt-qr.png" width="300" />
+            <h2>Austin JavaScript Shirts are $25</h2>
           </div>
         </div>
         <div className="quadrant">
           <div className="quadrant-content">
-            <h3>sponsored by</h3>
+            <h2>Sponsored by</h2>
+            <img src="/cloudflare.png"/>
           </div>
         </div>
       </div>

0dfed1e9dbbffcf5df9fb64b7178a02439ca8503
Kevin Kipp <kkipp@cloudflare.com>
2025-05-20 12:04:00 -0500

    May 2025

---
+++ src/slides/agenda-2025-05.tsx
@@ -0,0 +1,29 @@
+const Agenda202505 = () => {
+  return (
+    <section>
+      <div className="agendaWrapper">
+        <div className="agenda">
+          <h2>Agenda</h2>
+          <ol>
+            <li>Welcome!</li>
+            <li>
+              Lightning Talks
+              <ol>
+                <li>val.town</li>
+              </ol>
+            </li>
+            <li>Battledecks!</li>
+            <li>
+              <a href="https://docs.google.com/presentation/d/1FgcXPH4iqv90Nf1Ljl8wwaG8ZLolq_7FlPvDvK8zbtM">
+                Vibe Coding a Tamagotchi App
+              </a>
+            </li>
+            <li>Wrap up</li>
+          </ol>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Agenda202505;

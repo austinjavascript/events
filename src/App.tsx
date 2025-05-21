3a4083c554556fcbccad8698a49960e743034728
Kevin Kipp <kevin.kipp@gmail.com>
2025-05-21 12:48:32 -0500

    May 2025 (#8)
    * May 2025
    
    * Add Sponsors component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import { useEffect, useRef } from 'react'
-import Reveal from 'reveal.js'
-import Highlight from 'reveal.js/plugin/highlight/highlight'
-import Notes from 'reveal.js/plugin/notes/notes'
-import 'reveal.js/dist/reveal.css'
-import './styles/index.scss'
-import Splash from './slides/splash'
-import Agenda202504 from './slides/agenda-2025-04'
-import Thanks from './slides/thanks'
+import { useEffect, useRef } from "react";
+import Reveal from "reveal.js";
+import Highlight from "reveal.js/plugin/highlight/highlight";
+import Notes from "reveal.js/plugin/notes/notes";
+import "reveal.js/dist/reveal.css";
+import "./styles/index.scss";
+import Splash from "./slides/splash";
+import Agenda202505 from "./slides/agenda-2025-05";
+import Thanks from "./slides/thanks";
 
 function App() {
-  const deckDivRef = useRef<HTMLDivElement>(null) // reference to deck container div
-  const deckRef = useRef<Reveal.Api | null>(null) // reference to deck reveal instance
+  const deckDivRef = useRef<HTMLDivElement>(null); // reference to deck container div
+  const deckRef = useRef<Reveal.Api | null>(null); // reference to deck reveal instance
 
   useEffect(() => {
     // Prevents double initialization in strict mode
-    if (deckRef.current) return
+    if (deckRef.current) return;
 
     deckRef.current = new Reveal(deckDivRef.current!, {
-      transition: 'slide',
+      transition: "slide",
       // other config options
-    })
+    });
 
     deckRef.current
       .initialize({
@@ -28,29 +28,29 @@ function App() {
       })
       .then(() => {
         // good place for event handlers and plugin setups
-      })
+      });
 
     return () => {
       try {
         if (deckRef.current) {
-          deckRef.current.destroy()
-          deckRef.current = null
+          deckRef.current.destroy();
+          deckRef.current = null;
         }
       } catch (e) {
-        console.warn('Reveal.js destroy call failed.')
+        console.warn("Reveal.js destroy call failed.");
       }
-    }
-  }, [])
+    };
+  }, []);
 
   return (
     <div className="reveal" ref={deckDivRef}>
       <div className="slides">
         <Splash />
-        <Agenda202504 />
+        <Agenda202505 />
         <Thanks />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/slides/agenda-2025-05.tsx
@@ -0,0 +1,24 @@
+const Agenda202505 = () => {
+  return (
+    <section>
+      <div className="agendaWrapper">
+        <div className="agenda">
+          <h2>Agenda</h2>
+          <ol>
+            <li>Welcome!</li>
+            <li>Shirts!</li>
+            <li>Frontend Masters raffle</li>
+            <li>
+              <a href="https://docs.google.com/presentation/d/1FgcXPH4iqv90Nf1Ljl8wwaG8ZLolq_7FlPvDvK8zbtM">
+                Vibe Coding a Tamagotchi App
+              </a>
+            </li>
+            <li>Wrap up</li>
+          </ol>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Agenda202505;

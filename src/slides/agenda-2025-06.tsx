1bdad603acd2608dc741d7782f21e9503e702ada
Joah Gerstenberg <me@joahg.com>
2025-06-17 17:09:39 -0500

    add june meetup slides (#9)

---
+++ src/slides/agenda-2025-06.tsx
@@ -0,0 +1,28 @@
+const Agenda202505 = () => {
+  return (
+    <section>
+      <div className="agendaWrapper">
+        <div className="agenda">
+          <h2>Agenda</h2>
+          <ol>
+            <li>Welcome!</li>
+            <li>Shirts!</li>
+            <li>Frontend Masters raffle</li>
+            <li>
+              ⚡️ Accelerating migrations with AI
+            </li>
+            <li>
+              ⚡️ Nutrition for software engineers
+            </li>
+            <li>
+              🎤 Universal Tools For AI
+            </li>
+            <li>Wrap up</li>
+          </ol>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Agenda202505;

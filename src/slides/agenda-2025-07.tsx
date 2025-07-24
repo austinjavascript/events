34e587ffa08dd7a8f39ad9fafcc7f7dade76ce58
Kevin Kipp <kevin.kipp@gmail.com>
2025-07-24 12:53:49 -0500

    Add july 2025 slides (#10)

---
+++ src/slides/agenda-2025-07.tsx
@@ -0,0 +1,22 @@
+const Agenda202507 = () => {
+  return (
+    <section>
+      <div className="agendaWrapper">
+        <div className="agenda">
+          <h2>Agenda</h2>
+          <ol>
+            <li>Welcome!</li>
+            <li>Shirts!</li>
+            <li>⚡️ AI in Software Engineering</li>
+            <li>⚡️ Cloudflare Containers</li>
+            <li>⚡️ MCP's by orlie</li>
+            <li>🎤 Let’s vibecode a multiplayer web game with goose</li>
+            <li>Wrap up</li>
+          </ol>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Agenda202507;

98f83498d7093439ebab95038ddaf2df7e171690
Kevin Kipp <kkipp@cloudflare.com>
2025-12-16 13:35:55 -0600

    update slides for dec 2025

---
+++ src/slides/agenda-2025-12.tsx
@@ -0,0 +1,23 @@
+const Agenda202512 = () => {
+  return (
+    <section>
+      <div className="agendaWrapper">
+        <div className="agenda">
+          <h2>Agenda</h2>
+          <ol>
+            <li>Welcome!</li>
+            <li>Shirts!</li>
+            <li>⚡️ Organize, notate, and fold your code better in VS Code</li>
+            <li>⚡️ Coding in 26</li>
+            <li>⚡️ Jujutsu VCS</li>
+            <li>Battledecks</li>
+            <li>Trivia</li>
+            <li>Wrap up</li>
+          </ol>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Agenda202512;

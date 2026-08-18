e920f590c06310d3200ab385581a48a5286ec145
Lucas Castro <53352488+lucasamonrc@users.noreply.github.com>
2026-08-18 17:45:39 -0500

    Add aug agenda (#21)

---
+++ src/slides/agenda-2026-08.tsx
@@ -0,0 +1,22 @@
+const Agenda202608 = () => {
+  return (
+    <section>
+      <div className="agendaWrapper">
+        <div className="agenda">
+          <img src="https://c.tenor.com/LhIysipvdrIAAAAd/tenor.gif" alt="" />
+          <h2>Agenda</h2>
+          <ol>
+            <li>Welcome!</li>
+            <li>Shirts!</li>
+            <li>⚡ Onboard Agents like a member of your team with Sickr by Arif M.</li>
+            <li>🌩️ The Canary in the Codebase by Riley Hilliard</li>
+            <li>🌩️ Take the Keys Away From Your Agent by David Bowman</li>
+            <li>Wrap up</li>
+          </ol>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Agenda202608;
